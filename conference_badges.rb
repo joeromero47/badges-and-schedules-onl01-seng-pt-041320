b58caf0a50cac4fece9f49b251dec14c072823ec
Joseph Romero <joeromero47@gmail.com>
2020-04-21 02:27:04 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,8 +1,9 @@
 # Write your code here.
 def badge_maker (name)
-  puts "Hello, my name is #{"name"}."
+  puts "Hello, my name is #{name}."
 end
 
-
-
+speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+def batch_badge_creator
+  batch_badge_creator.each
   
